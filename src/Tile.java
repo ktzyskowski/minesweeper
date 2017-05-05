cf162362220f91225b42c33dbfd9359212fafd14
Kevin Zyskowski <Kevin@Kevins-MacBook-Pro.local>
2017-05-05 19:43:21 -0400

    Change Tile.java from interface to base class

--- src/Tile.java
+++ src/Tile.java
@@ -1,8 +1,31 @@
-public interface Tile {
+public class Tile
+{
+  private String type;
+
+  /** specific constructor of Tile class. To be called by subclasses
+    * in super() calls.
+   */
+  public Tile(String type)
+  {
+    this.type = type;
+
+  }
+
+  /** generic constructor to be used in construction of Tile objects.
+   */
+  public Tile()
+  {
+    super("Tile");
+
+  }
 
   /**
-    @return: returns type of tile
-  */
-  public String getTileType();
+    * @return returns type of Tile.
+   */
+  public String getTileType()
+  {
+    return type;
+
+  }
 
 }
