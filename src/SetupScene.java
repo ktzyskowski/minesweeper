adf51168eb21ac3d0f6baca3026b382d942b756e
dangreco <dan.greco@live.com>
2017-05-05 15:07:48 -0400

    Minor changes

--- src/SetupScene.java
+++ src/SetupScene.java
@@ -1,6 +1,7 @@
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
 import javafx.scene.layout.Pane;
+import javafx.scene.control.Label;
 
 public class SetupScene extends Scene {
 
@@ -8,11 +9,22 @@ public class SetupScene extends Scene {
   public static final int SCENEWIDTH = 500;
   public static final int SCENEHEIGHT = 500;
 
+  // Labels
+  private Label titleL, numRowsL, numColsL, developersL;
+
   public SetupScene()
   {
     super(null, SetupScene.SCENEWIDTH, SetupScene.SCENEHEIGHT);
     root = new Pane();
     this.setRoot(root);
+
+    titleL = new Label("Minesweeper v.0.1");
+    numRowsL = new Label("Number of rows:");
+    numColsL = new Label("Numver of cols:");
+    developersL = new Label("Developed by Kevin Zyskowski and Daniel Greco");
+
+    root.addAll
+
   }
 
 }
