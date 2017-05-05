3c24ca48db2ae0d39e607736bd1bc5886a730748
Kevin Zyskowski <Kevin@Kevins-MacBook-Pro.local>
2017-05-05 19:44:06 -0400

    Add Bomb and MinesweeperGame class

---
+++ src/Bomb.java
@@ -0,0 +1,12 @@
+public class Bomb extends Tile
+{
+
+  /** generic constructor to be used in construction of Bomb objects.
+   */
+  public Bomb()
+  {
+    super("Bomb");
+
+  }
+
+}
---
+++ src/MinesweeperGame.java
@@ -0,0 +1,60 @@
+public class MinesweeperGame
+{
+  private Tile[][] board;
+
+  /** constructor for a MinesweeperGame class.
+    * @param rows total number of rows in new board.
+    * @param cols total number of columns in new board.
+    * @param bombs total number of bombs in new board.
+   */
+  public MinesweeperGame(int rows, int cols, int bombs)
+  {
+    board = new Tile[rows][cols];
+
+    for (int r = 0; r < rows; r++)
+    {
+      for (int c = 0; c < cols; c++)
+      {
+        board[r][c] = new Tile();
+      }
+    }
+
+    // ... disperse bombs randomly
+    while (bombs > 0)
+    {
+      int randRow = (int)(Math.random() * rows);
+      int randCol = (int)(Math.random() * cols);
+      if (! board[randRow][randCol] instanceof Bomb)
+      {
+        board[randRow][randCol] = new Bomb();
+        bombs--;
+      }
+    }
+
+  }
+
+  /** method to return a number from 0 to 8 indicating the number of bombs
+    * around a specific tile.
+    * @return returns number in correspondance to number of bombs
+    * around the tile.
+    * @param row row of the tile to get the number for.
+    * @param col column of the tile to get the number for.
+   */
+  public getNumber(int row, int col)
+  {
+    int number = 0;
+    for (int r = row - 1; r <= row + 1; r++)
+    {
+      for (int c = col - 1; c <= col + 1; c++)
+      {
+        if (r >= 0 && c >= 0 && r < board.length && c < board[row].length && (r != row && c != col) && board[r][c] instanceof Bomb)
+        {
+            number++;
+        }
+      }
+    }
+    return number;
+
+  }
+
+}
