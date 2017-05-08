69d439cb61828d4b0b57b8aaf7e20d3fa9f7286e
dangreco <dan.greco@live.com>
2017-05-08 07:33:07 -0400

    Add NumberTile class

---
+++ src/NumberTile.java
@@ -0,0 +1,21 @@
+public class NumberTile extends Tile {
+
+    private int number;
+
+    public NumberTile(int number)
+    {
+        super();
+        this.number = number;
+    }
+
+    public int getNumber()
+    {
+        return number;
+    }
+
+    public void setNumber(int number)
+    {
+        this.number = number;
+    }
+
+}
